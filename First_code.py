88387db15c703e09c35afbbf286ad889f83e0b9d
amor <ashish.mor@idt.net>
2023-01-27 12:23:38 +0530

    code added

--- First_code.py
+++ First_code.py
@@ -1,1 +1,10 @@
-print("hello world")
+# https://www.youtube.com/watch?v=8ext9G7xspg
+# string concatenation ( how to put strings together)
+# suppose we want to create a string that says "subscribe to _______"
+
+# a few ways to do this 
+
+youtuber = " " #some string variable 
+
+print ("subscribe to " + youtuber)
+print ("subscribe to {}".format(youtuber))

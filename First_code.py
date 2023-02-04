c9a6af0bebcc6371f2a0ca4c999c6e4a35c50740
amor <ashish.mor@idt.net>
2023-02-04 23:25:12 +0530

    code added

--- First_code.py
+++ First_code.py
@@ -2,9 +2,23 @@
 # string concatenation ( how to put strings together)
 # suppose we want to create a string that says "subscribe to _______"
 
-# a few ways to do this 
-
-youtuber = " " #some string variable 
+# code 1 
+youtuber = "Ashish Mor" #some string variable 
 
 print ("subscribe to " + youtuber)
 print ("subscribe to {}".format(youtuber))
+print ( f"subscribe to {youtuber}") # F string 
+
+# Code 2 
+adj1 = input("Adjective1 : ")
+verb1 = input ("Verb1: ")
+verb2 = input("verb2: ")
+famous_person = input ("famous person: ") 
+
+madlib = f"Computer programming is no {adj1}! It makes me so excited all the time because. \
+I love to {verb2}. Stay hydrated and {verb2} like you are {famous_person}
+
+# code 3 
+
+
+# 872174830d46450185c97029f1adc6ad

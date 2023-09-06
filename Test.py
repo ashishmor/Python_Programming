22b31be14179bcb9654e562217dfb4b86048d651
ashishmor <morashish15@gmail.com>
2023-09-06 13:29:55 +0530

    updated

--- Test.py
+++ Test.py
@@ -1,19 +1,13 @@
-from datetime import datetime
+import pyautogui
 import time
-now = datetime.now()  # This does not require importing datetime
-
-
-
 
 while True:
-    # Move the cursor
-    now = datetime.now()
-    print(now)
-    
-    # Wait for 30 seconds
-    time.sleep(1)
+    # Get the current mouse cursor position
+    x, y = pyautogui.position()
 
+    # Simulate typing 'a' at the current cursor position
+    pyautogui.click(x, y)  # Click at the current position (to focus the input)
+    pyautogui.typewrite('a', interval=0.1)  # Type 'a' with a slight delay between keypresses
 
-
-        
-    
+    # Sleep for 5 seconds before the next 'a'
+    time.sleep(5)

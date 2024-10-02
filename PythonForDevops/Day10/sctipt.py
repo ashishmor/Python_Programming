3bdd285ab85bbca463c623594460c757ecb2a1a7
ashishmor <32420517+ashishmor@users.noreply.github.com>
2024-10-02 16:26:55 +0530

    update

---
+++ PythonForDevops/Day10/sctipt.py
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=cpgOgRxZ0r8&list=PLdpzxOOAlwvKwTyYNJCUwGPvql0TrsPgv&index=11
+
+# class ke andr def likhte hai ( these definitions are called methods ) , __init__ method bi hota 
+# hai jisme self use hota hai. init bi class ke method hi hua. 
+
+# Object oriented programming vs functional programming = OO are structured with objects and classes hoti hai, 
+# where we initiate the objects using calsses. 
+
+# Working with Lists
+
+#  Q - Provide list of all the files in the list of folders which user provides ?
+

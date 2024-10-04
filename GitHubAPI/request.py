10174a6920df8cc42cd339de423a28a6688c12c3
ashishmor <32420517+ashishmor@users.noreply.github.com>
2024-10-05 00:59:02 +0530

    d

--- GitHubAPI/request.py
+++ GitHubAPI/request.py
@@ -5,7 +5,7 @@ path = "user"
 token = 
 
 headers = { 
-    authorization : "Bearer {}".format (token)
+    "authorization : Bearer {}".format (token)
 }
 
 response = request.get(url.format(path), headers =headers)
